3616a52673db93f863b19599147be4d0d2cddeb9
muhammad junaid <junaidbishi02@gmail.com>
2025-03-16 11:00:05 +0500

    Update file-converter.py

--- file-converter.py
+++ file-converter.py
@@ -26,9 +26,9 @@ upload_files = st.file_uploader("upload your files(accepts CSV or Excel):",type=
 if upload_files:
     for file in upload_files:
         file_ext = os.path.splitext(file.name)[-1].lower()
-        
+
         if file_ext == ".csv":
-             df = pd.read_csv(file)
+            df = pd.read_csv(file)
             elif file_ext == ".xlsx":
                 df = pd.read_excel(file)
                 else:
